58089184498e3418e534f9ebfe3bbe4223699bea
Nileshlad <ladnilesh1111@gmail.com>
2020-03-12 14:22:28 +0530

    Added: To check the happy and sad

--- src/main/java/MoodAnalyserProblem.java
+++ src/main/java/MoodAnalyserProblem.java
@@ -1,5 +1,23 @@
 public class MoodAnalyserProblem {
+    //VARIABLE
+    String message;
+    String mood;
+
+    //METHOD
+    public String analysisMood(String message)
+    {
+        if(message.equals("i am happy mood"))
+        {
+            mood = "happy";
+        }else if(message.equals("i am sad mood")){
+            mood= "sad";
+        }
+        return mood;
+    }
     public static void main (String args[]){
         System.out.println("WELCOME MOOD ANALYSER PROBLEM");
+
     }
+
+
 }
--- src/test/java/MoodAnalyserProblemTest.java
+++ src/test/java/MoodAnalyserProblemTest.java
@@ -1,3 +1,23 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyserProblemTest {
 
+    //OBJECT
+    MoodAnalyserProblem moodAnalyser = new MoodAnalyserProblem();
+
+    //TEST CASE 1
+    @Test
+    public void givenMessage_WhenValid_ThenReturnMood() {
+       String result = moodAnalyser.analysisMood("i am happy mood");
+        Assert.assertEquals("happy",result);
+    }
+
+    //TEST CASE 2
+    @Test
+    public void givenMessage_WhenInvalid_ThenReturenMood() {
+        String result = moodAnalyser.analysisMood("i am sad mood");
+        Assert.assertEquals("sad",result);
+
+    }
 }
